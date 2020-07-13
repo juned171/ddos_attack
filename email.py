eb963771d996e1c576952e7cc3594699dc561ee0
juned171 <64685735+juned171@users.noreply.github.com>
2020-07-14 01:53:57 +0530

    Create email.py

---
+++ email.py
@@ -0,0 +1,33 @@
+import smtplib
+
+
+
+# In[8]:
+
+
+server=smtplib.SMTP_SSL("smtp.gmail.com",465)
+
+
+# In[10]:
+
+
+server.login("junedansari771@gmail.com","password")
+
+
+# In[11]:
+
+
+message = "IP blocked Successfully!!!"
+
+
+# In[12]:
+
+
+server.sendmail("junedansari771@gmail.com","junedansari771@gmail.com",message)
+
+
+# In[13]:
+
+
+server.quit()
+
